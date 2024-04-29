4c14e5250f7149b46d15be6a57120e46a27b4171
Francesco Torchia <francesco.torchia@suse.com>
2024-04-29 20:50:58 +0200

    Add Single product home; add labels
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/product.ts
+++ pkg/harvester/product.ts
@@ -1,10 +1,27 @@
 import { IPlugin } from '@shell/core/types';
-import { PRODUCT_NAME, BLANK_CLUSTER } from './config/harvester';
+import { PRODUCT_NAME, BLANK_CLUSTER, LOGO as logo } from './config/harvester';
+import { HCI } from './config/types';
 
 export function init($plugin: IPlugin, store: any) {
   const { product } = $plugin.DSL(store, PRODUCT_NAME);
 
   const isSingleProduct = process.env.rancherEnv === PRODUCT_NAME;
+
+  if (isSingleProduct) {
+    const home = {
+      name:      `${ PRODUCT_NAME }-c-cluster`,
+      path:      `/${ PRODUCT_NAME }/c/:cluster`
+    };
+
+    store.dispatch('setIsSingleProduct', {
+      productNameKey:    'harvester.productLabel',
+      getVersionInfo:    (store: any) => store.getters[`${ PRODUCT_NAME }/byId`]?.(HCI.SETTING, 'server-version')?.value || 'unknown',
+      afterLoginRoute:   home,
+      logoRoute:         home,
+      supportCustomLogo: true,
+      logo,
+    });
+  }
 
   product({
     inStore:               'management',
