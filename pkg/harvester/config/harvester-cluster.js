7c04b1417a380267f1e027d8a61cc47f9c00ecdc
Francesco Torchia <francesco.torchia@suse.com>
2024-09-08 18:08:03 +0200

    Update USB devices
    - Add columns and filters
    - Filter other vm devices in pci and usb devices pages when selecting devices
    
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/config/harvester-cluster.js
+++ pkg/harvester/config/harvester-cluster.js
@@ -771,6 +771,14 @@ export function init($plugin, store) {
     hiddenNamespaceGroupButton: true,
     listGroups:                 [
       {
+        icon:       'icon-list-grouped',
+        value:      'description',
+        field:      'groupByDevice',
+        hideColumn: 'description',
+        tooltipKey: 'resourceTable.groupBy.device'
+      },
+      {
+        icon:       'icon-cluster',
         value:      'node',
         field:      'groupByNode',
         hideColumn: 'node',
