ab79ed310c6434a1058d97542a8ccf06f04053b5
Cody Jackson <cody.jackson@suse.com>
2024-09-12 14:58:03 -0700

    Adding in fixes to get harvester cluster pages rendering

--- pkg/harvester/index.ts
+++ pkg/harvester/index.ts
@@ -1,6 +1,9 @@
 import { importTypes } from '@rancher/auto-import';
 import { IPlugin } from '@shell/core/types';
 import extensionRoutes from './routing/harvester-routing';
+import harvesterCommonStore from './store/harvester-common';
+import harvesterStore from './store/harvester-store';
+import customValidators from './validators';
 
 // Init the package
 export default function (plugin: IPlugin) {
@@ -14,5 +17,11 @@ export default function (plugin: IPlugin) {
   plugin.metadata.icon = require('./icon.svg');
 
   plugin.addProduct(require('./config/harvester-manager'));
+  plugin.addProduct(require('./config/harvester-cluster'));
+
+  plugin.addDashboardStore(harvesterCommonStore.config.namespace, harvesterCommonStore.specifics, harvesterCommonStore.config);
+  plugin.addDashboardStore(harvesterStore.config.namespace, harvesterStore.specifics, harvesterStore.config, harvesterStore.init);
+  plugin.validators = customValidators;
+
   plugin.addRoutes(extensionRoutes);
 }
