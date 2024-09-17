2ec5ebb373be75fe0ef5249b5a11350a217ed1ec
Francesco Torchia <francesco.torchia@suse.com>
2024-09-17 21:59:12 +0200

    Refactoring devices models
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/edit/kubevirt.io.virtualmachine/VirtualMachinePciDevices/index.vue
+++ pkg/harvester/edit/kubevirt.io.virtualmachine/VirtualMachinePciDevices/index.vue
@@ -7,7 +7,7 @@ import CompatibilityMatrix from '../CompatibilityMatrix';
 import DeviceList from './DeviceList';
 
 import remove from 'lodash/remove';
-import { get, set } from '@shell/utils/object';
+import { set } from '@shell/utils/object';
 import { HCI } from '../../../types';
 import DeviceList from './DeviceList';
 import CompatibilityMatrix from './CompatibilityMatrix';
@@ -123,9 +123,8 @@ export default {
         if (vm.metadata.name === this.vm?.metadata?.name) {
           return inUse;
         }
-        const devices = get(vm, 'spec.template.spec.domain.devices.hostDevices') || [];
 
-        devices.forEach((device) => {
+        vm.hostDevices.forEach((device) => {
           inUse[device.name] = { usedBy: [vm.metadata.name] };
         });
 
@@ -136,19 +135,14 @@ export default {
     },
 
     devicesByNode() {
-      const out = {};
+      return this.enabledDevices?.reduce((acc, device) => {
+        const name = device.status?.nodeName;
 
-      this.enabledDevices.forEach((deviceCRD) => {
-        const nodeName = deviceCRD.status?.nodeName;
-
-        if (!out[nodeName]) {
-          out[nodeName] = [deviceCRD];
-        } else {
-          out[nodeName].push(deviceCRD);
-        }
-      });
-
-      return out;
+        return name ? {
+          ...acc,
+          [name]: [...(acc[name] || []), device],
+        } : acc;
+      }, {});
     },
 
     // determine which nodes contain all devices selected
