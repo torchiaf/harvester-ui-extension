52e3254f83c4f60ea6c33be8d2588be669f3ab6e
Francesco Torchia <francesco.torchia@suse.com>
2024-10-01 15:29:36 +0200

    Fix HarvesterQuotaDialog
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/dialog/HarvesterQuotaDialog.vue
+++ pkg/harvester/dialog/HarvesterQuotaDialog.vue
@@ -5,6 +5,7 @@ import { Banner } from '@components/Banner';
 import AsyncButton from '@shell/components/AsyncButton';
 import UnitInput from '@shell/components/form/UnitInput';
 import { exceptionToErrorsArray } from '@shell/utils/error';
+import { _EDIT } from '@shell/config/query-params';
 
 export default {
   name: 'HarvesterVMQuotaDialog',
@@ -29,6 +30,7 @@ export default {
 
   data() {
     return {
+      _EDIT,
       totalSnapshotSize: '',
       errors:            []
     };
@@ -90,7 +92,7 @@ export default {
         v-int-number
         :label="t('harvester.snapshot.totalSnapshotSize')"
         :disabled="false"
-        :mode="create"
+        :mode="_EDIT"
         :input-exponent="3"
         :increment="1024"
         :output-modifier="true"
