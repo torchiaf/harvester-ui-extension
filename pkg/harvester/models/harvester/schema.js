0e21c157fac8e8ef025c6a8bd46ba3bfcde91013
Francesco Torchia <francesco.torchia@suse.com>
2024-11-15 00:53:28 +0100

    remote-code

--- pkg/harvester/models/harvester/schema.js
+++
@@ -1,6 +0,0 @@
-import SteveSchema from '@shell/models/steve-schema';
-
-/**
- * This is the steve schema that's used for the harvester store
- */
-export default class HarvesterStoreSchema extends SteveSchema { }
