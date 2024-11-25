c57990e7add31415036d4b51f88cb0070fdab832
Francesco Torchia <francesco.torchia@suse.com>
2024-11-25 19:36:23 +0100

    temp3

--- pkg/harvester/models/harvester/schema.js
+++ pkg/harvester/models/harvester/schema.js
@@ -1,6 +1,6 @@
-import SteveSchema from '@shell/models/steve-schema';
+import ManagementStoreSchema from '@shell/models/management/schema';
 
 /**
  * This is the steve schema that's used for the harvester store
  */
-export default class HarvesterStoreSchema extends SteveSchema { }
+export default class HarvesterStoreSchema extends ManagementStoreSchema { }
