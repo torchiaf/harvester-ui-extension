ba60dd6e3f1ccb8d879d0ce28f77cd1149ab437e
Francesco Torchia <francesco.torchia@suse.com>
2024-11-25 16:51:52 +0100

    lint

--- pkg/harvester/models/schema.js
+++ pkg/harvester/models/schema.js
@@ -4,7 +4,7 @@ import SteveSchema from '@shell/models/steve-schema';
  * This is the steve schema that's used for the harvester store
  */
 export default class HarvesterStoreSchema extends SteveSchema {
-    constructor(data, ctx, rehydrateNamespace = null, setClone = false) {
-        super(data, ctx, rehydrateNamespace, setClone);
-    }
+  constructor(data, ctx, rehydrateNamespace = null, setClone = false) {
+    super(data, ctx, rehydrateNamespace, setClone);
+  }
 }
