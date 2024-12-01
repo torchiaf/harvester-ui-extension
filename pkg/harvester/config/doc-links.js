7572d62a67bbfa34f712b2a03f62c77d0f109c00
Francesco Torchia <francesco.torchia@suse.com>
2024-12-01 15:19:23 +0100

    build

--- pkg/harvester/config/doc-links.js
+++
@@ -1,8 +0,0 @@
-// suffix of doc link, see utils/feature-flags.js how to get complete doc link
-export const DOC = {
-  CONSOLE_URL:             `/host/#remote-console`,
-  RANCHER_INTEGRATION_URL: `/rancher/rancher-integration`,
-  STORAGE_NETWORK_EXAMPLE: `/advanced/storagenetwork#configuration-example`,
-  KSMTUNED_MODE:           `/host/#ksmtuned-mode`,
-  UPGRADE_URL:             `/upgrade/index`
-};
